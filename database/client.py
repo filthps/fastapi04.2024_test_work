564511f3b93d27a3d3a6d1e4f3089119e5607507
Victor-PC <filpsixi@mail.ru>
2024-05-11 07:52:03 +0300

    abstractions

--- database/client.py
+++ database/client.py
@@ -1,15 +1,39 @@
-from abc import ABC, abstractmethod
-from dotenv
+from http.server import HTTPServer
+from typing import Callable
+from dotenv import dotenv_values
 
 
-class Connection(ABC):
+conf = dotenv_values(dotenv_path="../../conf.env")
 
 
-    def __init__(self):
+class APi:
+    _server = None
+    path_roots = {
+        "select_item": ...,
+        "insert_item": ...,
+        "request_sal_token": ...,
+
+    }
+
+    def __new__(cls, *args, **kwargs):
+        ...  # http.server.HTTPServer
+
+    def select_item(self, api_url, name, model_name=None):
         ...
 
+    def insert_item(self):
+        ...
 
-class Client:
+    def __getattribute__(self, item, *args, **kwargs) -> Callable:
+        return super().__getattribute__(item)(self.path_roots[item])
+
+
+class DatabaseClient:
+    _connection = None
+
+    def __new__(cls):
+        ...  # sqlite3.connection
+
     def insert(self, table_name):
         pass
 
--- database/models.py
+++ database/models.py
@@ -1,34 +1,62 @@
-from typing import Optional
+import sys
+from typing import Union
 
 
 class Model:
-    def __init__(self):
-        self._insert = False
-        self._update = False
-        self._delete = False
-
-    def __str__(self):
+    def __init__(self, primary_key="autoincr", insert=False, update=False, delete=False, **kwargs):
+        self._primary_key: Union[dict, str] = primary_key  # Python >= 3.7 required! (dict keys ordering)
+        self._insert = insert
+        self._update = update
+        self._delete = delete
+        self._data = kwargs
         self.__is_valid()
 
+    @property
+    def insert(self) -> str:
+        data = self._data.copy()
+        data.update(self._primary_key)
+        return f"INSERT INTO ({', '.join(data.keys())}) VALUES ({', '.join(data.values())});"
 
-    def insert(self):
-        self._insert = True
+    @property
+    def update(self) -> str:
+        return f"UPDATE {self.__class__.__name__} SET {', '.join(map(lambda i: ' = '.join(i), self._data.items()))} " \
+               f"WHERE {' = '.join(tuple(self._primary_key.items())[0])};"
 
-    def update(self):
-        self._update = True
+    @property
+    def delete(self) -> str:
+        return f"DELETE FROM {self.__class__.__name__} WHERE {' = '.join(tuple(self._primary_key.items())[0])};"
 
-    def delete(self):
-        self._delete = True
+    def __str__(self):
+        if self._insert:
+            return self.insert
+        if self._update:
+            return self.update
+        if self._delete:
+            return self.delete
+        return "Invalid"
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.__str__()})"
 
     def __is_valid(self):
+        if not (sys.version_info.major == 3 and sys.version_info.minor) >= 7:
+            raise RuntimeError
         if sum([self._delete, self._update, self._insert]) != 1:
             raise ValueError
+        if not isinstance(self._primary_key, (dict, str,)):
+            raise TypeError
+        if type(self._primary_key) is str:
+            if self._primary_key != "autoincr":
+                raise TypeError
+        if len(self._primary_key) != 1:
+            raise ValueError
+        if self._update or self._insert:
+            if not self._data:
+                raise ValueError
 
 
 class Sallary(Model):
-    def __init__(self, **data, pk: Optional[dict] = None):
-        self._data = data
-        super().__init__()
+    pass
 
 
 class Employee(Model):
@@ -36,4 +64,4 @@ class Employee(Model):
 
 
 class Bookkeeping(Model):
-    ...
+    pass
