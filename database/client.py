c496cb303a34e2462874d2193424000245890811
Victor-PC <filpsixi@mail.ru>
2024-05-19 07:27:52 +0300

    Мысли о csrf token

--- database/client.py
+++ database/client.py
@@ -1,23 +1,23 @@
+import uuid
 from typing import Callable
 from dotenv import dotenv_values
 from fastapi import FastAPI
+from cryptography.fernet import Fernet
 from database.sqlite.init import connection, BASE_DIR
 from database.models import Sallary, Bookkeeping, Employee
 
 conf = dotenv_values(dotenv_path="../conf.env")
 fastapi = FastAPI()
+clipper = ...  # Держим это в секрете
 
 
-class API:
-    path_roots = {
-        "add_employee": "emp/add-emp/",
-        "dismiss_employee": "emp/dis-emp/",
-        "issue_wages": "sallary/issue/",
-        "change_sallary": "sallary/change/",
-        "request_sal_token": "token/request/",
-        "destroy_token": "token/close/",
-        "change_bookkeeping": "bookkeeping/change/",
-    }
+@fastapi.get("auth/{ui_id}")
+def get_token(ui_id):
+    """  """
+    session_key = Fernet.generate_key()
+    fernet_instance = Fernet(session_key)
+    token = fernet_instance.encrypt(clipper)  # fernet_instance.decrypt(token)
+    return token
 
 
 @fastapi.post("emp/add-emp/{emp_id}")
@@ -35,18 +35,21 @@ def issue_wages(self):
     ...
 
 
+@fastapi.post("sallary/change/{emp_id}")
 def change_sallary(self):
     pass
 
 
+@fastapi.post("token/request/{emp_id}")
 def request_sal_token(self):
     pass
 
 
+@fastapi.post("token/close/{token}")
 def destroy_token(self):
     pass
 
 
+@fastapi.post("bookkeeping/change/")
 def change_bookkeeping(self):
     pass
-
